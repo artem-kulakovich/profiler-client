3397c460beb460a3e90aae2f97cb16c621bb6c53
=artem-kulakovich <=artemka228-1@mail.ru>
2023-05-06 12:58:41 +0300

    change migration

--- src/main/resources/db/migration/V1_2__create_data.sql
+++ src/main/resources/db/migration/V1_2__create_data.sql
@@ -14,4 +14,4 @@ INSERT INTO public."roles_permissions"(role_id, permission_id)
 VALUES (1, 2);
 
 INSERT INTO public."user"(user_name, password, email, create_at, listener_id, role_id)
-VALUES ('admin', 'YWRtaW4=', 'artemkulakovich237@gmail.com', '2023-04-14T16:20:02.320253', 0, 1);
+VALUES ('admin', 'YWRtaW4=', 'artemkulakovich237@gmail.com', '2023-04-14T16:20:02.320253', NULL, 1);
