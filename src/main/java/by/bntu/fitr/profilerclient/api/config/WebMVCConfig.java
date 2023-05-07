a4a2dac7def42a9d779547387e92529291e19d12
=artem-kulakovich <=artemka228-1@mail.ru>
2023-05-07 14:28:19 +0300

    add cors issues

--- src/main/java/by/bntu/fitr/profilerclient/api/config/WebMVCConfig.java
+++ src/main/java/by/bntu/fitr/profilerclient/api/config/WebMVCConfig.java
@@ -10,6 +10,6 @@ public class WebMVCConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 }
